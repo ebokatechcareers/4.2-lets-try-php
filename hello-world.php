3c1efac0f060e0997d7f7e14324f06769330dbea
eboka <ebokacp@yahoo.com>
2020-09-30 12:01:41 -0600

    learning php

---
+++ hello-world.php
@@ -0,0 +1,19 @@
+This is before my PHP tag.
+
+
+<?php 
+// "Echo" and "print" are functions we can...
+//use to output strings as a response.
+echo "Hello, World";
+
+//There are two other stykes of comment we can use in PHP. This (//) is single line
+
+    # this is also a single line comment
+
+/* This is our
+       (familiar)
+            multi-line
+    comment! */
+
+$studentName = "\nFahad";
+echo $studentName;
